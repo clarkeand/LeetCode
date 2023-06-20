1cde48abcea8240c4c4907c59d688b7a67344431
clarkeand <superclarke909@gmail.com>
2023-06-19 20:02:14 -0600

    added June19ths problem

---
+++ Problems_June19th/problems.py
@@ -0,0 +1,45 @@
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+"""
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_dict = {}
+        
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in num_dict:
+                return [num_dict[diff], i]
+            num_dict[num] = i
+        
+        # if no pair adds up to the target
+        return []
+    
+"""
+There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
+
+You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+"""
+
+class Solution(object):
+    def largestAltitude(self, gain):
+        """
+        :type gain: List[int]
+        :rtype: int
+        """
+        current_elevation = 0
+        highest_altitude = 0
+        for elevation in gain:
+            current_elevation += elevation
+            if current_elevation > highest_altitude: 
+                highest_altitude = current_elevation
+        return highest_altitude
