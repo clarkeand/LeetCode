81a055082deafd5d003ace8b53ba9c3b3b286ba6
clarkeand <superclarke909@gmail.com>
2023-06-27 08:59:48 -0600

    June27

--- Week June26/problems.py
+++ Week June26/problems.py
@@ -13,3 +13,33 @@ def solution(number):
         x += 1
         
     return sum
+
+"""In this kata you have to write a simple Morse code decoder. While the Morse code is now mostly superseded by voice and digital data communication channels, it still has its use in some applications around the world.
+The Morse code encodes every character as a sequence of "dots" and "dashes". For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive, traditionally capital letters are used. When the message is written in Morse code, a single space is used to separate the character codes and 3 spaces are used to separate words. For example, the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
+
+NOTE: Extra spaces before or after the code have no meaning and should be ignored.
+
+In addition to letters, digits and some punctuation, there are some special service codes, the most notorious of those is the international distress signal SOS (that was first issued by Titanic), that is coded as ···−−−···. These special codes are treated as single special characters, and usually are transmitted as separate words."""
+
+from preloaded import MORSE_CODE
+
+def decode_morse(morse_code):
+    # Remember - you can use the preloaded MORSE_CODE dictionary:
+    # For example: 
+    # MORSE_CODE['.-'] = 'A'
+    # MORSE_CODE['--...'] = '7'
+    # MORSE_CODE['...-..-'] = '$'
+    
+    morse_list = morse_code.split(" ")
+    return_word = ""
+    dbl_space = 0
+    for morse in morse_list: 
+        if morse == "" and dbl_space == 1:
+            return_word += " "
+            dbl_space = 0
+        elif morse == "": 
+            dbl_space += 1
+        else:
+            return_word += MORSE_CODE[morse]
+        
+    return return_word.lstrip().rstrip()
