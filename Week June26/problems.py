b6833679b5116b23d19e2a7f7120f4c4aef8880a
clarkeand <superclarke909@gmail.com>
2023-07-01 16:14:24 -0600

    problem July 1st

--- Week June26/problems.py
+++ Week June26/problems.py
@@ -43,3 +43,34 @@ def decode_morse(morse_code):
             return_word += MORSE_CODE[morse]
         
     return return_word.lstrip().rstrip()
+
+"""
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+"""
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        x = str(x)
+        new_num_list = []
+        return_num = ""
+        
+        for digit in x:
+            new_num_list.append(digit)
+
+        if new_num_list[0] == '-':
+            return_num += new_num_list.pop(0)
+
+        while new_num_list != []:
+            return_num += new_num_list.pop()
+
+        return_num = int(return_num)
+
+        if return_num >= 2147483647 or return_num <= -2147483648: 
+            return 0
+    
+        return return_num
