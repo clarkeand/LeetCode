424bb6e5274d1e6604db250bd9cfd40a3600e87d
clarkeand <superclarke909@gmail.com>
2023-07-03 15:45:08 -0600

    more problems

--- Week_Jul3/problems.py
+++ Week_Jul3/problems.py
@@ -23,4 +23,30 @@ def to_camel_case(text):
         camel_case += text[0].upper()+text[1:]
         
     return camel_case
+
+"""
+Define a function that takes an integer argument and returns a logical value true or false depending on if the integer is a prime.
+
+Per Wikipedia, a prime number ( or a prime ) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+Requirements
+You can assume you will be given an integer input.
+You can not assume that the integer will be only positive. You may be given negative numbers as well ( or 0 ).
+NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. Numbers go up to 2^31 ( or similar, depending on language ). Looping all the way up to n, or n/2, will be too slow.
+"""
         
+def is_prime(num):
+    
+    if num <= 0 or num == 1: 
+        return False
+    
+    i = 1
+    
+    while i < 100000: 
+        if num % i == 0 and i != 1:
+            return False
+        i += 1
+        if i == num: 
+            return True
+        
+    return True
