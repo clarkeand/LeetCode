c095de2fe8a376f2ba1df12b4624c26aa8079128
clarkeand <superclarke909@gmail.com>
2023-07-04 11:43:04 -0600

    mo code mo problems

--- Week_Jul3/problems.py
+++ Week_Jul3/problems.py
@@ -186,3 +186,38 @@ def is_pangram(s):
     if len(alpha_set) == 0: 
         return True
     return False
+
+"""The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+such as
+
+F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+F(n) * F(n+1) = prod.
+
+Your function productFib takes an integer (prod) and returns an array:
+
+[F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+depending on the language if F(n) * F(n+1) = prod.
+
+If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+[F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+F(n) being the smallest one such as F(n) * F(n+1) > prod."""
+
+def productFib(prod):
+    fib_array = [0, 1]  # Initial values of the Fibonacci sequence
+
+    # Generate Fibonacci numbers dynamically until prod is reached or surpassed
+    while fib_array[-1] * fib_array[-2] < prod:
+        fib_array.append(fib_array[-1] + fib_array[-2])
+
+    # Check if the product is equal to prod
+    if fib_array[-1] * fib_array[-2] == prod:
+        return [fib_array[-2], fib_array[-1], True]
+    else:
+        return [fib_array[-2], fib_array[-1], False]
