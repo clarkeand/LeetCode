b0422b5d37a41fc192e652983f3788c28240a841
clarkeand <superclarke909@gmail.com>
2023-07-16 22:44:21 -0700

    js problem

--- Week_Jul10/problems.js
+++ Week_Jul10/problems.js
@@ -15,3 +15,29 @@ function digitalRoot(n) {
     }
     return Number(stringN);
 }
+
+/*Count the number of Duplicates
+Write a function that will return the count of distinct case-insensitive alphabetic characters 
+and numeric digits that occur more than once in the input string. The input string can be assumed 
+to contain only alphabets (both uppercase and lowercase) and numeric digits.*/
+
+function duplicateCount(text) {
+  let newString = "";
+  let counter = 0;
+  let duplicateString = "";
+  
+  for (let i = 0; i < text.length; i++) {
+    let lowerCaseChar = text[i].toLowerCase();
+    
+    // Check if the character is in the new string but not in the duplicate string
+    if (newString.includes(lowerCaseChar) && !duplicateString.includes(lowerCaseChar)) {
+      counter += 1;
+      duplicateString += lowerCaseChar; // Add the character to the duplicate string
+    } 
+    else if (!newString.includes(lowerCaseChar)) {
+      newString += lowerCaseChar;
+    }
+  }
+  
+  return counter;
+}
