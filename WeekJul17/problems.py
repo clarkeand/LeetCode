45b136703640e801ab6ca082097169e4767bde44
clarkeand <superclarke909@gmail.com>
2023-07-27 17:58:00 -0600

    py problem

--- WeekJul17/problems.py
+++ WeekJul17/problems.py
@@ -14,3 +14,51 @@ def cakes(recipe, available):
         if recipe_quotient < min_cakes: 
             min_cakes = recipe_quotient
     return min_cakes
+
+"""In this kata we want to convert a string into an integer. The strings simply represent the numbers in words.
+
+Examples:
+
+"one" => 1
+"twenty" => 20
+"two hundred forty-six" => 246"""
+
+def parse_int(string):
+    num_map = {
+        "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4,
+        "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9,
+        "ten": 10, "eleven": 11, "twelve": 12, "thirteen": 13, "fourteen": 14,
+        "fifteen": 15, "sixteen": 16, "seventeen": 17, "eighteen": 18, "nineteen": 19,
+        "twenty": 20, "thirty": 30, "forty": 40, "fifty": 50, "sixty": 60,
+        "seventy": 70, "eighty": 80, "ninety": 90,
+        "hundred": 100, "thousand": 1000, "million": 1000000
+    }
+
+    split_words = string.replace("-", " ").split()
+    result = 0
+    current_number = 0
+    last_multiplier = 1
+
+    for word in split_words:
+        if word == "and":
+            continue
+
+        # Handle compound numbers with hyphens
+        word = word.replace("-", "")
+
+        if word in num_map:
+            value = num_map[word]
+
+            # Handle multipliers (hundred, thousand, million)
+            if value in [100, 1000, 1000000]:
+                if value == 100 and current_number != 0:
+                    current_number *= value
+                else:
+                    result += current_number * value
+                    last_multiplier = value
+                    current_number = 0
+            else:
+                current_number += value
+
+    result += current_number
+    return result
