90ee1043170d326eab88f1988540537e6eeb8723
clarkeand <superclarke909@gmail.com>
2023-08-14 23:10:49 -0600

    js problem

---
+++ August2023/problems.js
@@ -0,0 +1,9 @@
+//Given a positive integer millis, write an asynchronous function 
+//that sleeps for millis milliseconds. It can resolve any value.
+
+/*
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    return new Promise(resolve => setTimeout(resolve, millis));
+}
